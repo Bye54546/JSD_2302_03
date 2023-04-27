c5b513ed5d34c5cd8479605a8c0c8737765e2f7f
Clifton Hills <1966839714@qq.com>
2023-04-27 18:38:13 +0800

    0425

---
+++ weibotext/src/main/java/cn/tedu/weibotext/WeibotextApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.weibotext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeibotextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeibotextApplication.class, args);
+    }
+
+}
---
+++ weibotext/src/test/java/cn/tedu/weibotext/WeibotextApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.weibotext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeibotextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
